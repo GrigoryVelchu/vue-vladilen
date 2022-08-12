b9c736f7339363cc87aa7e8a6a6473c96e90047f
GrigoryVelchy <veldog2323@yandex.ru>
2022-08-12 15:47:14 +0300

    simple note app +v-bind v-on v-if v-for info +methods,computed,watch info

--- app.js
+++ app.js
@@ -1,16 +1,54 @@
 const App = {
     data() {
         return {
-            count: 0
+            title: 'Список заметок: ',
+            placeholderValue: 'Введите название заметки',
+            inputValue: '',
+            notes: [
+                'Note 1',
+                'Note 2',
+                'Note 3'
+            ]
         }
     },
+    //
     methods: {
-        increment() {
-            this.count++
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
         },
-        decrement() {
-            this.count--
+        addNote() {
+            this.inputValue.trim().length > 0 && this.notes.push(this.inputValue)
+            this.inputValue = ''
         },
+        // inputKeypress(event){
+        //     if(event.key === 'Enter'){this.addNote()}
+        // },
+        deleteNote(idx, event) {
+            console.log(event)
+            this.notes.splice(idx, 1)
+        },
+        textToUpperCase(text) {
+            return text.toUpperCase()
+        },
+    },
+    //computed возвращают значения на основании той модели состояния компонента(data), которая используется в методе, следят за ней Являются геттерами и сеттерами, не вызываются а передаются как переменные
+    computed:{
+        doubleCountComputed() {
+            console.log('doubleCountComputed method')
+            return this.notes.length * 2;
+        },
+    },
+    //watch позволяет нам наблюдать за одноименной моделью состояния
+    watch:{
+        inputValue(){
+            const mat = ['хуй', 'пизда', 'бля']
+            mat.forEach(word=>{
+                if(this.inputValue.includes(word)){
+                    this.inputValue = ""
+                    alert('Не ругайся, подонок')
+                }
+            })
+        }
     }
 }
 Vue.createApp(App).mount("#app")
